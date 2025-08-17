b2c281ab2ef2e0b37ec2a2b7593392170c77ee91
Xavier Murtagh Molina <90108226+xavimurtagh@users.noreply.github.com>
2025-08-17 16:21:38 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,3 +11,21 @@ export default function App({
     </SessionProvider>
   )
 }
+
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+function Header() {
+  const { data: session } = useSession();
+  return (
+    <div className="header">
+      {/* other nav/tabs here */}
+      <div className="auth-buttons">
+        {session ? (
+          <button onClick={() => signOut()}>Sign Out</button>
+        ) : (
+          <button onClick={() => signIn()}>Sign In</button>
+        )}
+      </div>
+    </div>
+  );
+}
