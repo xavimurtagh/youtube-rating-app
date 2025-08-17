045d26a91ddde259b52f3404fb2d5a57bfafed6d
Xavier Murtagh Molina <90108226+xavimurtagh@users.noreply.github.com>
2025-08-17 12:11:17 +0100

    Add files via upload

---
+++ pages/api/nextauth-api-route.js
@@ -0,0 +1,36 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'openid email profile https://www.googleapis.com/auth/youtube.readonly'
+        }
+      }
+    })
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token
+        token.refreshToken = account.refresh_token
+      }
+      return token
+    },
+    async session({ session, token }) {
+      // Send properties to the client
+      session.accessToken = token.accessToken
+      session.refreshToken = token.refreshToken
+      return session
+    },
+  },
+  pages: {
+    signIn: '/auth/signin',
+    error: '/auth/error',
+  }
+})
