51d86b39b704d2405e72d024e5914485d51163df
Xavier Murtagh Molina <90108226+xavimurtagh@users.noreply.github.com>
2025-08-17 13:14:06 +0100

    Update nextauth.js

--- pages/api/auth/nextauth.js
+++ pages/api/auth/nextauth.js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -15,7 +15,6 @@ export default NextAuth({
   ],
   callbacks: {
     async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token
         token.refreshToken = account.refresh_token
@@ -23,14 +22,15 @@ export default NextAuth({
       return token
     },
     async session({ session, token }) {
-      // Send properties to the client
       session.accessToken = token.accessToken
       session.refreshToken = token.refreshToken
       return session
     },
   },
   pages: {
-    signIn: '/auth/signin',
     error: '/auth/error',
-  }
-})
+  },
+  debug: process.env.NODE_ENV === 'development',
+}
+
+export default NextAuth(authOptions)
