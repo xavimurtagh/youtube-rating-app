d19550c62aa06ed121ba05161151d81d0a7fb609
Xavier Murtagh Molina <90108226+xavimurtagh@users.noreply.github.com>
2025-08-22 17:17:43 +0100

    Update rate.ts

--- pages/api/rate.ts
+++ pages/api/rate.ts
@@ -7,6 +7,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
+  console.log('Request body:', req.body);
+
+
   try {
     const me = await getUser(req, res);
     if (!me) {
