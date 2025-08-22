c5411cba0f7f76ca7a4b4a715cf5592b9817abe2
Xavier Murtagh Molina <90108226+xavimurtagh@users.noreply.github.com>
2025-08-22 22:32:32 +0100

    Update rate.ts

--- pages/api/rate.ts
+++ pages/api/rate.ts
@@ -3,10 +3,6 @@ import { prisma } from '../../lib/prisma';
 import { getUser } from '../../lib/auth';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
-
   if (req.method === 'DELETE') {
     try {
       const me = await getUser(req, res);
