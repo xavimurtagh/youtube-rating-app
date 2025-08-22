3d6303eb9dee1bd2ab880a465106b48c56dba4ac
Xavier Murtagh Molina <90108226+xavimurtagh@users.noreply.github.com>
2025-08-22 16:36:55 +0100

    Update rate.ts

--- pages/api/auth/rate.ts
+++ pages/api/auth/rate.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '../../lib/prisma';
-import { getUser } from '../../lib/auth';
+import { getUser } from '../../../lib/auth';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
