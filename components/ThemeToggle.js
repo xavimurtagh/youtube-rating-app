10bf73e88873aa378fdc6b0a5fdc5f430fea4ae1
Xavier Murtagh Molina <90108226+xavimurtagh@users.noreply.github.com>
2025-08-29 22:08:42 +0100

    Remove global styles import in ThemeToggle.js

--- components/ThemeToggle.js
+++ components/ThemeToggle.js
@@ -1,6 +1,5 @@
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
-import styles from '../styles/globals.css';
 
 export default function ThemeToggle() {
   const [mounted, setMounted] = useState(false);
