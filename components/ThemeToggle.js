ee1727efc6a8cd77e7274272c637bb82530cbe5c
Xavier Murtagh Molina <90108226+xavimurtagh@users.noreply.github.com>
2025-08-29 18:35:24 +0100

    Update ThemeToggle.js

--- components/ThemeToggle.js
+++ components/ThemeToggle.js
@@ -1,6 +1,6 @@
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
-import styles from '../styles/ThemeToggle.module.css';
+import styles from '../styles/globals.css';
 
 export default function ThemeToggle() {
   const [mounted, setMounted] = useState(false);
