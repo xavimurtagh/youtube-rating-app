88196803700586000e1fc2ea2f6041cb15ef69e8
Xavier Murtagh Molina <90108226+xavimurtagh@users.noreply.github.com>
2025-09-02 11:55:58 +0100

    Fix follower data clearing in clear-all-data API

--- pages/api/clear-all-data.ts
+++ pages/api/clear-all-data.ts
@@ -29,7 +29,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         where: { id: me.id },
         data: {
           following: { set: [] },
-          followedBy: { set: [] },
+          followers: { set: [] },
           favourites: { set: [] }
         }
       })
