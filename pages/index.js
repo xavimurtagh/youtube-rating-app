0c497e296f0e13cd2b9fd3896a0c4375329c91a5
Xavier Murtagh Molina <90108226+xavimurtagh@users.noreply.github.com>
2025-09-02 18:01:42 +0100

    Add Liberapay donation script and image

--- pages/index.js
+++ pages/index.js
@@ -463,6 +463,8 @@ export default function Home() {
               </ul>
             </div>
           </div>
+          <script src="https://liberapay.com/xavstermur/widgets/button.js"></script>
+          <noscript><a href="https://liberapay.com/xavstermur/donate"><img alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg"></a></noscript>
         )}
       </main>
     </>
