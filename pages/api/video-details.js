c4e85b7a5a933bf0f7226bb34b082e3148ffd0f3
Xavier Murtagh Molina <90108226+xavimurtagh@users.noreply.github.com>
2025-09-03 18:34:00 +0100

    Change video description to an empty string

--- pages/api/video-details.js
+++ pages/api/video-details.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
           title: details.title,
           channel: details.channel,
           thumbnail: details.thumbnail,
-          description: details.description,
+          description: '',
           isMusic: false, // You can enhance this logic
         });
       } catch (error) {
