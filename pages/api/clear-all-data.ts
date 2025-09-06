35d92a6780bc3b9f102d0b844099d7fe700eabfe
Xavier Murtagh Molina <90108226+xavimurtagh@users.noreply.github.com>
2025-09-06 13:49:09 +0100

    Fix favourites clearing syntax in clear-all-data API

--- pages/api/clear-all-data.ts
+++ pages/api/clear-all-data.ts
@@ -33,7 +33,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       await tx.user.update({
         where: { id: me.id },
           data: {
-            favourites: { [] }
+            favourites: { set:[] }
           }
       })
 
